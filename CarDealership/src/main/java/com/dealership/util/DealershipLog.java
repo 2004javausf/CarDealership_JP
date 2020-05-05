95d44f5cd6e56f3cafd7fccbf06a55541982db44
jpark1914 <parkerj4321@gmail.com>
2020-05-05 18:49:45 -0500

    Set up project structure

---
+++ CarDealership/src/main/java/com/dealership/util/DealershipLog.java
@@ -0,0 +1,44 @@
+package com.dealership.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class DealershipLog {
+
+		static Logger logger = LogManager.getLogger();
+		
+		public static void LogIt(String level, String message) {
+			/*
+			 * debug
+			 * warn
+			 * error
+			 * fatal
+			 * info
+			 * trace
+			 * 
+			 */
+			switch(level) {
+				case "debug":
+					logger.debug(message);
+					break;
+				case "warn":
+					logger.warn(message);
+					break;
+				case "error":
+					logger.error(message);
+					break;
+				case "fatal":
+					logger.fatal(message);
+					break;
+				case "info":
+					logger.info(message);
+					break;
+				case "trace":
+					logger.trace(message);
+					break;
+				default:
+					System.out.println("logger dumb");
+			}
+		}
+
+}
